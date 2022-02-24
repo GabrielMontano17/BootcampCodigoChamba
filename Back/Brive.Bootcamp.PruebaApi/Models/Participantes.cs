577bd4e1e47645099ef420b62478ffbad51ef727
José Cassiel Agüero Gloria <CassielAG@github.com>
2022-02-24 00:17:33 -0600

    Organizar front y back

---
+++ Back/Brive.Bootcamp.PruebaApi/Models/Participantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brive.Bootcamp.PruebaApi.Models
+{
+    public class Participantes
+    {
+        public string Nombre { get; set;}
+        public int Edad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
---
+++ Back/Brive.Bootcamp.PruebaApi/Servicios/IParticipantes.cs
@@ -0,0 +1,13 @@
+﻿using Brive.Bootcamp.PruebaApi.Models;
+
+namespace Brive.Bootcamp.PruebaApi.Servicios
+{
+    public interface IParticipantes
+    {
+        bool GuardarParticipante(Participantes participantes);
+
+        Participantes[] ObtenerParticipantes();
+
+        Participantes[] ObtenerParticipantesMayoresDeEdad();
+    }
+}
